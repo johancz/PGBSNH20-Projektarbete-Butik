c1a211e3a4101f626202ce405f3e86d6d5494486
JoCh <johancz@users.noreply.github.com>
2020-11-02 17:57:41 +0100

    [wip-save] DO NOT MERGE INTO MASTER-BRANCH
    This spaghetti-code should not be merged into the master-branch, I'm just saving my work.
    Savedatetime: 2020-11-02 17:57

--- Butik-User/MainWindow.xaml.cs
+++ Butik-User/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
@@ -50,17 +51,97 @@ namespace Butik_User
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
 
             CreateUIElements();
-            ProductsListbox = new ListBox();
-            DrawProductListbox();
+            //ProductsListbox = new ListBox();
+            //DrawProductListbox();
 
-            SizeChanged += MainWindow_OnSizeChanged;
+            //SizeChanged += MainWindow_OnSizeChanged;
+            KeyUp += OnKeyUp;
+        }
+
+        //private TabItem CreateMockupTabItem(HeaderedContentControl header, ContentControl content)
+        private TabItem CreateMockupTabItem(string header, UIElement content)
+        {
+            var border = new Border
+            {
+                BorderThickness = new Thickness(0, 1, 0, 0),
+                BorderBrush = Brushes.Black,
+            };
+
+            var tabItem = new TabItem
+            {
+                Header = header,
+                Content = border
+            };
+            border.Child = content;
+
+            return tabItem;
         }
 
         private void CreateUIElements()
         {
+            ////////////////////////// [START] REMOVE BEFORE "RELEASE"
+            {
+                try
+                {
+                    Background = new SolidColorBrush(GetChromeColor().Value);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Could not create a SolidColorBrush from color created with GetChromeColor()");
+                }
+
+            }
+            ////////////////////////// [END] REMOVE BEFORE "RELEASE"
+
             // Root element
             // Tabs
             Tabs = new TabControl();
+            Tabs.Padding = new Thickness(-1, 5, -1, -1);
+
+            // Mockup 1
+            {
+                // Mockup content
+                var tabs = new TabControl();
+                var tabItem1 = new TabItem
+                {
+                    Header = "Browse Products"
+                };
+                var tabItem2 = new TabItem
+                {
+                    Header = "My Shopping Cart"
+                };
+
+                var tabItem1_rootGrid = new Grid { ShowGridLines = true };
+                tabItem1_rootGrid.RowDefinitions.Add(new RowDefinition());
+                tabItem1_rootGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                tabItem1_rootGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                tabItem1.Content = tabItem1_rootGrid;
+
+                tabs.Items.Add(tabItem1);
+                tabs.Items.Add(tabItem2);
+
+                // Mockup tabitem
+                Tabs.Items.Add(CreateMockupTabItem("Mockup 1", tabs));
+            }
+
+            // Mockup 2
+            {
+                var tabItem_mockup2 = new TabItem();
+                tabItem_mockup2.Header = "Mockup 2";
+
+                var grid = new Grid
+                {
+                    ShowGridLines = true,
+                };
+                grid.RowDefinitions.Add(new RowDefinition());
+                grid.RowDefinitions.Add(new RowDefinition());
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+
+                // Mockup tabitem
+                Tabs.Items.Add(CreateMockupTabItem("Mockup 2", grid));
+            }
+
             var tabItem_products = new TabItem();
             tabItem_products.Header = "Products";
             var tabItem_shoppingCart = new TabItem();
@@ -214,6 +295,64 @@ namespace Butik_User
             grid.Children.Add(control);
         }
 
+        private StackPanel Mockup1_CreateProductGridItem(Product product)
+        {
+            var stackPanel = new StackPanel
+            {
+                Orientation = Orientation.Vertical,
+
+            };
+
+            return stackPanel;
+        }
+
+        private Image CreateNewImage(string uriString, int height, string tooltipText = null, bool largerImageInTooltip = false)
+        {
+            Image image = new Image();
+            try
+            {
+                var uri = new Uri(uriString, UriKind.Relative);
+                image.Source = new BitmapImage(uri);
+                image.HorizontalAlignment = HorizontalAlignment.Center;
+                image.VerticalAlignment = VerticalAlignment.Center;
+                image.Margin = new Thickness(5);
+                image.Stretch = Stretch.Uniform;
+                image.Height = 50;
+
+                if (tooltipText != null || largerImageInTooltip != false)
+                {
+                    var tooltipStackpanel = new StackPanel { Orientation = Orientation.Vertical };
+                    image.ToolTip = tooltipStackpanel;
+
+                    if (tooltipText != null)
+                    {
+                        tooltipStackpanel.Children.Add(new Label
+                        {
+                            Content = tooltipText
+                        });
+                    }
+
+                    if (largerImageInTooltip != false)
+                    {
+                        tooltipStackpanel.Children.Add(new Image
+                        {
+                            Source = new BitmapImage(uri),
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            VerticalAlignment = VerticalAlignment.Center,
+                            Margin = new Thickness(5),
+                            Stretch = Stretch.Uniform,
+                        });
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // TODO(johancz): exception-handling
+            }
+
+            return image;
+        }
+
         private ListBoxItem? CreateListboxItem(Product product)
         {
             ///////////// REPLACE WITH DATAGRID? //////////////////////////////////////////////////////////////////////////////////
@@ -222,34 +361,35 @@ namespace Butik_User
                 Orientation = Orientation.Horizontal
             };
 
-            Image image = new Image();
-            try
-            {
-                var uri = new Uri("/Images/broccoli-1238250_640.jpg", UriKind.Relative);
-                var bmi = new BitmapImage(uri);
+            Image image = CreateNewImage("/Images/broccoli-1238250_640.jpg", 50, null, true);
+            //Image image = new Image();
+            //try
+            //{
+            //    var uri = new Uri("/Images/broccoli-1238250_640.jpg", UriKind.Relative);
+            //    var bmi = new BitmapImage(uri);
 
-                image.Source = new BitmapImage(uri);
-                image.HorizontalAlignment = HorizontalAlignment.Center;
-                image.VerticalAlignment = VerticalAlignment.Center;
-                image.Margin = new Thickness(5);
-                image.Stretch = Stretch.Uniform;
-                image.Height = 50;
-                image.ToolTip = new ToolTip
-                {
-                    Content = new Image
-                    {
-                        Source = bmi,
-                        HorizontalAlignment = HorizontalAlignment.Center,
-                        VerticalAlignment = VerticalAlignment.Center,
-                        Margin = new Thickness(5),
-                        Stretch = Stretch.Uniform,
-                    }
-                };
-            }
-            catch (Exception e)
-            {
-                // TODO(johancz): exception-handling
-            }
+            //    image.Source = new BitmapImage(uri);
+            //    image.HorizontalAlignment = HorizontalAlignment.Center;
+            //    image.VerticalAlignment = VerticalAlignment.Center;
+            //    image.Margin = new Thickness(5);
+            //    image.Stretch = Stretch.Uniform;
+            //    image.Height = 50;
+            //    image.ToolTip = new ToolTip
+            //    {
+            //        Content = new Image
+            //        {
+            //            Source = bmi,
+            //            HorizontalAlignment = HorizontalAlignment.Center,
+            //            VerticalAlignment = VerticalAlignment.Center,
+            //            Margin = new Thickness(5),
+            //            Stretch = Stretch.Uniform,
+            //        }
+            //    };
+            //}
+            //catch (Exception e)
+            //{
+            //    // TODO(johancz): exception-handling
+            //}
 
             stackPanel.Children.Add(image);
             ///////////// REPLACE WITH DATAGRID? //////////////////////////////////////////////////////////////////////////////////
@@ -271,5 +411,83 @@ namespace Butik_User
             Grid.SetColumn(image, column);
             grid.Children.Add(image);
         }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        ////////////////////////// [START] REMOVE BEFORE "RELEASE"
+        public static Color? GetChromeColor()
+        {
+            bool isEnabled;
+            var hr1 = DwmIsCompositionEnabled(out isEnabled);
+            if ((hr1 != 0) || !isEnabled) // 0 means S_OK.
+                return null;
+
+            DWMCOLORIZATIONPARAMS parameters;
+            try
+            {
+                // This API is undocumented and so may become unusable in future versions of OSes.
+                var hr2 = DwmGetColorizationParameters(out parameters);
+                if (hr2 != 0) // 0 means S_OK.
+                    return null;
+            }
+            catch
+            {
+                return null;
+            }
+
+            // Convert colorization color parameter to Color ignoring alpha channel.
+            var targetColor = Color.FromRgb(
+                (byte)(parameters.colorizationColor >> 16),
+                (byte)(parameters.colorizationColor >> 8),
+                (byte)parameters.colorizationColor);
+
+            // Prepare base gray color.
+            var baseColor = Color.FromRgb(217, 217, 217);
+
+            // Blend the two colors using colorization color balance parameter.
+            return BlendColor(targetColor, baseColor, (double)(100 - parameters.colorizationColorBalance));
+        }
+
+        private static Color BlendColor(Color color1, Color color2, double color2Perc)
+        {
+            if ((color2Perc < 0) || (100 < color2Perc))
+                throw new ArgumentOutOfRangeException("color2Perc");
+
+            return Color.FromRgb(
+                BlendColorChannel(color1.R, color2.R, color2Perc),
+                BlendColorChannel(color1.G, color2.G, color2Perc),
+                BlendColorChannel(color1.B, color2.B, color2Perc));
+        }
+
+        private static byte BlendColorChannel(double channel1, double channel2, double channel2Perc)
+        {
+            var buff = channel1 + (channel2 - channel1) * channel2Perc / 100D;
+            return Math.Min((byte)Math.Round(buff), (byte)255);
+        }
+
+        [DllImport("Dwmapi.dll")]
+        private static extern int DwmIsCompositionEnabled([MarshalAs(UnmanagedType.Bool)] out bool pfEnabled);
+
+        [DllImport("Dwmapi.dll", EntryPoint = "#127")] // Undocumented API
+        private static extern int DwmGetColorizationParameters(out DWMCOLORIZATIONPARAMS parameters);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct DWMCOLORIZATIONPARAMS
+        {
+            public uint colorizationColor;
+            public uint colorizationAfterglow;
+            public uint colorizationColorBalance; // Ranging from 0 to 100
+            public uint colorizationAfterglowBalance;
+            public uint colorizationBlurBalance;
+            public uint colorizationGlassReflectionIntensity;
+            public uint colorizationOpaqueBlend;
+        }
+        ////////////////////////// [END] REMOVE BEFORE "RELEASE"
     }
 }
