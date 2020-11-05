b7ff81f31e2476d9fbf57d3c752747cfc1b4917b
RobinAxelsson <axelsson_1991@hotmail.com>
2020-11-05 16:33:23 +0100

    doit2

--- Butik-User/Product.cs
+++ Butik-User/Product.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows.Controls;
 
@@ -9,9 +12,39 @@ namespace Butik_User
     {
         public string Name { get; set; }
         public string Description;
-        public double Price { get; set; }
-        public Uri ImageUri; // TODO(johancz): change to "string ImageUriString"?
+        public decimal Price { get; set; }
+        public string Uri;
 
-        public Product() { }
+        public Product(string name, string uri, decimal price, string description)
+        {
+            Name = name;
+            Uri = uri;
+            Price = price;
+            Description = description;
+        }
+
+        public void ToFile(string filePath)
+        {
+            File.AppendAllText(filePath, $"{Name},{Uri},{Price},{Description}" + Environment.NewLine);
+        }
+
+        public static List<Product> LoadAll(string filePath)
+        {
+            var products = new List<Product>();
+            var lines = File.ReadAllLines(filePath).ToList();
+            foreach (var l in lines)
+            {
+                var property = l.Split(',');
+
+                var name = property[0];
+                var uri = property[1];
+                var price = decimal.Parse(property[2]);
+                var description = property[3];
+                var newProduct = new Product(name, uri, price, description);
+                products.Add(newProduct);
+            }
+
+            return products;
+        }
     }
 }
