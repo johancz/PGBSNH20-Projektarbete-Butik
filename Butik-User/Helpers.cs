36b4d90a552e6d912a4d70eca5cb5e42d58cff92
JoCh <johancz@users.noreply.github.com>
2020-11-07 18:57:54 +0100

    Move Helpers-class to a file of its own

---
+++ Butik-User/Helpers.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Butik_User
+{
+    public static class Helpers
+    {
+        public static BitmapImage CreateBitmapImageFromUriString(string uriString)
+        {
+            try
+            {
+                var uri = new Uri(@"Images\" + uriString, UriKind.Relative);
+                var bitMapImage = new BitmapImage(uri);
+
+                return bitMapImage;
+            }
+            catch (Exception)
+            {
+                // TODO(johancz): exception-handling
+                return null;
+            }
+        }
+        public static Image CreateNewImage(string uriString = null, int? height = null, string tooltipText = null, bool imageInTooltip = false)
+        {
+            Image image = new Image();
+
+            try
+            {
+                if (uriString != null)
+                {
+                    image.Source = CreateBitmapImageFromUriString(uriString);
+                }
+                image.HorizontalAlignment = HorizontalAlignment.Center;
+                image.VerticalAlignment = VerticalAlignment.Center;
+                image.Margin = new Thickness(5);
+                image.Stretch = Stretch.Uniform;
+
+                if (height != null)
+                {
+                    image.Height = height.Value;
+                }
+
+                if (tooltipText != null || imageInTooltip != false)
+                {
+                    var tooltipStackpanel = new StackPanel { Orientation = Orientation.Vertical };
+                    image.ToolTip = tooltipStackpanel;
+
+                    if (tooltipText != null)
+                    {
+                        tooltipStackpanel.Children.Add(new Label
+                        {
+                            Content = tooltipText
+                        });
+                    }
+
+                    if (imageInTooltip != false)
+                    {
+                        // TODO(johancz): Use the available Helper for creating images.
+                        tooltipStackpanel.Children.Add(new Image
+                        {
+                            Source = CreateBitmapImageFromUriString(uriString),
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            VerticalAlignment = VerticalAlignment.Center,
+                            Margin = new Thickness(5),
+                            Stretch = Stretch.Uniform,
+                        });
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // TODO(johancz): exception-handling
+            }
+
+            return image;
+        }
+    }
+}
