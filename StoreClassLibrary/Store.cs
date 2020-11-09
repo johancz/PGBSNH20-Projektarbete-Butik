6f1776f21df3ac052c449693471b3c051f69a0b1
RobinAxelsson <axelsson_1991@hotmail.com>
2020-11-09 08:42:25 +0100

    some leftouts pushed before merge attempt

--- StoreClassLibrary/Store.cs
+++ StoreClassLibrary/Store.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace StoreClassLibrary
 {
         public static class Store
         {
-            public static List<Product> Products { get; set; } = new List<Product>();
+
+            public static string TextFolderPath = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.FullName + "\\";
+            public static List<Product> Products { get; set; } = LoadProducts("LoadProducts.csv");
             public static ShoppingCart ActiveShoppingCart { get; set; } = new ShoppingCart();
+            //public static ShoppingCart shoppingCart = LoadShoppingCart("ShoppingCart.csv");
             public static List<DiscountCode> DiscountCodes { get; set; }
 
             public static void Init()
             {
-                Products = Product.LoadAll();
                 LoadShoppingCart();
                 LoadDiscountCodes();
             }
@@ -26,6 +29,29 @@ namespace StoreClassLibrary
             {
                 //DiscountCodes = ...
             }
-        }
+            public static List<Product> LoadProducts(string fileName)
+            {
+                var products = new List<Product>();
+                string input = File.ReadAllText(TextFolderPath + fileName);
+
+                var infoArray = input.Trim().Split('#');
+
+                for (int i = 0; i < infoArray.Length; i++)
+                {
+                    if (infoArray[i] == "") { break; }
+                    var name = infoArray[i].Trim();
+                    i++;
+                    var uri = infoArray[i].Trim();
+                    i++;
+                    var price = decimal.Parse(infoArray[i].Trim());
+                    i++;
+                    var description = infoArray[i].Trim();
+
+                    var newProduct = new Product(name, uri, price, description);
+                    products.Add(newProduct);
+                }
+                return products;
+            }
+    }
     
 }
