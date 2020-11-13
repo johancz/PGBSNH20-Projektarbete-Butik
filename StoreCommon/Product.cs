d219dc2269c8ba7a4ed5981c54449dbc3fe4aae1
JoCh <johancz@users.noreply.github.com>
2020-11-14 00:26:15 +0100

    Add property:"Currency" to class:"Product".
    - A Tuple with two properties: "Code" (e.g. "SEK"), Symbol (e.g. " kr").

--- StoreCommon/Product.cs
+++ StoreCommon/Product.cs
@@ -5,13 +5,15 @@
         public string Name { get; set; }
         public string Description;
         public decimal Price { get; set; }
+        public (string Code, string Symbol) Currency { get; set; }
         public string Uri;
 
-        public Product(string name, string uri, decimal price, string description)
+        public Product(string name, string uri, decimal price, string description, (string Code, string Symbol)? currency = null)
         {
             Name = name;
             Uri = uri;
             Price = price;
+            Currency = currency ?? (Code: "SEK", Symbol: " kr");
             Description = description;
         }
     }
